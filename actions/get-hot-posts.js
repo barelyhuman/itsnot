3e33d501b02647b55c1db6791cb3c5f950d82c98
sidgroks@gmail.com <sidgroks@gmail.com>
2020-08-01 15:08:28 +0530

    remove till i implement pagination

--- actions/get-hot-posts.js
+++ actions/get-hot-posts.js
@@ -4,11 +4,9 @@ export default async function getHotPosts(offset = 0, limit = 10) {
   try {
     const db = getDBInstance();
 
-    const data = await db('posts')
-      .where({})
-      .orderBy('votes', 'desc')
-      .offset(offset)
-      .limit(limit);
+    const data = await db('posts').where({}).orderBy('votes', 'desc');
+    // .offset(offset)
+    // .limit(limit);
 
     await db.destroy();
     return data;
